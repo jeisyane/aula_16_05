09ae9b36646ba5a747e00c1c5c45e8decedfe0e6
jeisyane <160151590+jeisyane@users.noreply.github.com>
2025-05-16 12:10:42 -0300

    Add files via upload

---
+++ atividade1.js
@@ -0,0 +1,4 @@
+const String = '{"nome": "Jeisy", "idade": 17, "cidade": "Curitiba"}';
+const objeto = JSON.parse(String);
+console.log(objeto.nome); // Jeisy
+
---
+++ atividade2.js
@@ -0,0 +1,8 @@
+const livro = {
+    titulo:"Assim que acaba",
+    autor:"Colleen Hoover",
+    ano:2018
+};
+
+const string = JSON.stringify(livro);
+console.log(string);
